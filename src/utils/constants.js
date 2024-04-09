bc90e36c07e471daae2c65ec8ee13e6cabe9d716
Anmol Sah <kumarxxx242424@gmail.com>
2024-04-09 20:00:33 +0530

    final commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -66,4 +66,4 @@ export const API = {
   ];
 
 
-  export const GEMINI_KEY = process.env.REACT_APP_GEMINI_KEY;
+  //export const GEMINI_KEY = process.env.REACT_APP_GEMINI_KEY;
--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -1,8 +1,8 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { GEMINI_KEY } from "./constants";
+//import { GEMINI_KEY } from "./constants";
 
 // Access your API key (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(GEMINI_KEY);
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_KEY);
 
 
 export default genAI; 
