db864f735049d88d9a55e78f382ec2e01d76ec51
Anmol Sah <kumarxxx242424@gmail.com>
2024-04-09 11:46:53 +0530

    final commit

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,6 +2,7 @@ import useNowPlayingMovies from "../hooks/useNowPlayingMovies"
 import usePopularMovies from "../hooks/usePopularMovies";
 import useTopTrendingMovies from "../hooks/useTopTrendingMovies";
 import useUpcomingMovies from "../hooks/useUpcomingMovies";
+import useWatchVideos from "../hooks/useWatchVideos";
 import GptSearch from "./GptSearch";
 import Header from "./Header"
 import MainContainer from "./MainContainer";
