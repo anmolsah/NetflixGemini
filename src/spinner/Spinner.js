2cdb41a662eb0743de93243da16ecc309cce4178
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-10 19:11:51 +0530

    refactor Spinner component for improved readability and consistency

--- src/spinner/Spinner.js
+++ src/spinner/Spinner.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import './Spinner.css';
+import React from "react";
+import "./Spinner.css";
 
 const Spinner = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-[70]">
-    <div className="loader"></div>
-  </div>
-  
-  
-  )
-}
+      <div className="loader"></div>
+    </div>
+  );
+};
 
-export default Spinner
+export default Spinner;
